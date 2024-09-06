4935393f00f70031f47597cfd538c1c615ab7486
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-09-06 11:47:03 -0300

    modulo 2 passando argumentos para o programa, e utilizando make

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+using namespace std;
 
-int main() {
+int main(int arg_count, char *args[]) {
 
-    std::cout << "Hello World" << std::endl;
+    //cout << "Hello World" << endl;
+
+    if(arg_count > 1) {
+        string name = args[1];
+
+        cout << "name is " << name << endl;    
+
+    }
+    else {
+         cout << "No Arguments" << endl;
+    }
+
     return 0;
 }

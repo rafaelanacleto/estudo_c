c59d7d386a70d23dbdaeb593010f659ede7ac2bc
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-09-06 19:18:49 -0300

    concluido list simple em C++ 100 % funcional

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 void print_menu(string name);
@@ -7,21 +8,28 @@ void add_item();
 void delete_item();
 void search_item();
 
-int main(int arg_count, char *args[]) {
+vector<string> list;
+string name;
 
-    if(arg_count > 1) {
-        string name = args[1];
-        cout << "Welcome  " << name << endl;  
-        print_menu(name); 
+int main(int arg_count, char *args[])
+{
+
+    if (arg_count > 1)
+    {
+        name = string(args[1]);
+        cout << "Welcome  " << name << endl;
+        print_menu(name);
     }
-    else {
-         cout << "No Arguments" << endl;
+    else
+    {
+        cout << "No Arguments" << endl;
     }
 
     return 0;
 }
 
-void print_menu(string name) {
+void print_menu(string name)
+{
     int choice;
 
     cout << "**********************************************************\n";
@@ -29,14 +37,104 @@ void print_menu(string name) {
     cout << " 2 - Add to list. \n";
     cout << " 3 - Delete from list. \n";
     cout << " 4 - Search in list. \n";
-    cout << " 5 - Quit. \n";
-    cout << " Enter your choice and press return/enter. \n";
-    cout << "**********************************************************\n" << endl;
+    cout << " 5 - Clear Screen. \n";
+    cout << " 6 - Quit. \n\n";
+    cout << " ->  Enter your choice and press return/enter. \n";
+    cout << "**********************************************************\n"
+         << endl;
 
     cin >> choice;
-    
-    if(choice == 5) {
-    	exit(0);    
+
+    if (choice == 6)
+    {
+        exit(0);
     }
-    
+    else if (choice == 2)
+    {
+        system("clear");
+        add_item();
+    }
+    else if (choice == 3)
+    {
+        system("clear");
+        delete_item();
+    }
+    else if (choice == 1)
+    {
+        system("clear");
+        print_list();
+    }
+    else if (choice == 5)
+    {
+        system("clear");
+        print_menu(name);
+    }
+}
+
+void add_item()
+{
+
+    cout << "\n";
+    cout << "***** Add Item *****\n";
+    cout << "Type in an item and press Enter: ";
+
+    string item;
+    cin >> item;
+
+    list.push_back(item);
+
+    cout << "\nSuccess added! \n";
+    cin.clear();
+
+    print_menu(name);
+}
+
+void print_list()
+{
+
+    cout << "\n\n";
+    cout << "***** Print List *****\n";
+    cout << "\n";
+    int c = 1;
+
+    for (string k : list)
+    {
+        cout << c++ << " - " << k << " | ";
+    }
+    cout << "\n\n\n";
+    print_menu(name);
+}
+
+void delete_item()
+{
+
+    int index = 0;
+    cout << "\n\n";
+    cout << "***** Delete Item *****\n";    
+
+    if (list.size())
+    {
+        for (int i = 0; i < list.size(); i++)
+        {
+            cout << i << " - " << list[i] << "\n";
+        }
+
+        cout << "-> Select an item index number to delete:\n";
+        cin >> index;
+    }
+    else
+    {
+        cout << "No item in the list.\n";
+    }
+
+    list.erase(list.begin() + index);
+    for (int i = 0; i < list.size(); i++)
+    {
+        cout << i << " - " << list[i] << "\n";
+    }
+
+    cout << "Item deleting success! \n\n\n";
+
+    print_menu(name);
+
 }

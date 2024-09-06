2f3c8830f63665bc3472bb35aa827616c8c7d674
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-09-06 19:23:30 -0300

    unsigned para reduzir o tamanho do var i de inteiro

--- src/main.cpp
+++ src/main.cpp
@@ -114,7 +114,7 @@ void delete_item()
 
     if (list.size())
     {
-        for (int i = 0; i < int(list.size()); i++)
+        for (unsigned int i = 0; i < int(list.size()); i++)
         {
             cout << i << " - " << list[i] << "\n";
         }
@@ -128,7 +128,7 @@ void delete_item()
     }
 
     list.erase(list.begin() + index);
-    for (int i = 0; i < int(list.size()); i++)
+    for (unsigned int i = 0; i < int(list.size()); i++)
     {
         cout << i << " - " << list[i] << "\n";
     }

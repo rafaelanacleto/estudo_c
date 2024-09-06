a6ad25f365f5c630ca2ed0e58428487bc049ecbb
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-09-06 14:09:52 -0300

    criando metodo de menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int main(int arg_count, char *args[]) {
 
-    //cout << "Hello World" << endl;
+void print_menu() {
+    
+}
+
+
+int main(int arg_count, char *args[]) {
 
     if(arg_count > 1) {
         string name = args[1];
-
-        cout << "name is " << name << endl;    
-
+        cout << "name is " << name << endl;   
     }
     else {
          cout << "No Arguments" << endl;
